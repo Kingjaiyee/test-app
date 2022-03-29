669bf3a5b7b2bac5d6282b8dd8f456318991288a
feyintoluwa <toluaina3@gmail.com>
2022-03-29 15:51:43 +0100

    Test app and deployemnt manifest to K8

--- planA/views.py
+++ planA/views.py
@@ -1,16 +1,15 @@
 from rest_framework.response import Response
 from rest_framework import generics
 from django.utils import timezone
-import httpagentparser
 from django.db import connection
 
 
 class GetUserTime(generics.ListAPIView):
+    # call the GET endpoint to get user info
     def get(self, request, *args, **kwargs):
+        # get the timezone of the user
         get_user_time = timezone.now()
         get_user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
-        # host_name = request.META["HTTP_USER_AGENT"]
-        # host_name_get = httpagentparser.detect(host_name)['os']['name']
         if get_user_ip:
             ip = get_user_ip.split(',')[0]
         else:
